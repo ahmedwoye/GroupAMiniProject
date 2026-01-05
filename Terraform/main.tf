ad9d3d437773c0aa2869c07f45222e581391dc70
ahmedwoye@gmail.com Woye1234@@ <ahmedwoye@gmail.com>
2026-01-05 16:56:15 +0000

    Terraform start File and Ignore File added to the code

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,14 @@
 terraform {
+
+
+
+  backend "s3" {
+    bucket  = "teachbleat-cicd-state-bucket"
+    key     = "envs/dev/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
